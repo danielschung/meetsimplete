d7d165e5690ede77ed71f1e5844208fb7c9112da
Daniel.Chung <dchung@NS72649-HA.local>
2018-10-25 17:45:03 -0500

    added faq styling and fixed minor updates

---
+++ js/faq.js
@@ -0,0 +1,31 @@
+$(document).ready (function() {
+
+	$("div.faq-header-text-container.1").first().on('click', function() {
+		$("div.faq-text-container.1").slideToggle(200);
+	});
+
+	$("div.faq-header-text-container.2").first().on('click', function() {
+		$("div.faq-text-container.2").slideToggle(200);
+	});
+
+	$("div.faq-header-text-container.3").first().on('click', function() {
+		$("div.faq-text-container.3").slideToggle(200);
+	});
+
+	$("div.faq-header-text-container.4").first().on('click', function() {
+		$("div.faq-text-container.4").slideToggle(200);
+	});
+
+	$("div.faq-header-text-container.5").first().on('click', function() {
+		$("div.faq-text-container.5").slideToggle(200);
+	});
+
+	$("div.faq-header-text-container.6").first().on('click', function() {
+		$("div.faq-text-container.6").slideToggle(200);
+	});
+
+	$("div.faq-header-text-container.7").first().on('click', function() {
+		$("div.faq-text-container.7").slideToggle(200);
+	});
+
+});
